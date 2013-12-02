08c499a3d66d30ee3a8e577c10cd6963a3eac0ff
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2013-12-02 13:41:12 -0500

    Format change to repo files for gpgkeyfile

--- manifests/hwtools.pp
+++ manifests/hwtools.pp
@@ -47,7 +47,7 @@ class dell::hwtools {
         mirrorlist => "${dell::omsa_url_base}${dell::omsa_version}/mirrors.cgi?${dell::omsa_url_args_indep}",
         enabled    => 1,
         gpgcheck   => 1,
-        gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n\tfile:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
+        gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n    file:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
         require    => [
           File['/etc/pki/rpm-gpg/RPM-GPG-KEY-dell'],
           File['/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios'],
